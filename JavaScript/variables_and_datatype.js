ee0ba22ebedec04a66a0ac21fdabef57d58528e0
SureshBabu01 <sureshbabub.time@gmail.com>
2024-03-22 19:33:03 +0530

    create JavaScript

---
+++ JavaScript/variables_and_datatype.js
@@ -0,0 +1,22 @@
+var a=10
+let b=20
+const c=30
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+var name="Suresh Babu"
+console.log(name)
+
+var r
+var h=0
+
+let d=true
+
+console.log("Types of Datatypes : ")
+console.log(typeof(a))
+console.log(typeof(name))
+console.log(typeof(r))
+console.log(typeof(h))
+console.log(typeof(d))
