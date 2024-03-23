653947d7ba8e6ac80357d6d2f1bb40a263285bf0
SureshBabu01 <sureshbabub.time@gmail.com>
2024-03-23 22:10:35 +0530

    create js

--- JavaScript/condition.js
+++ JavaScript/condition.js
@@ -1,9 +1,26 @@
-n=0;
+week=2;
 
-if(n==0){
-	console.log("True");
+if (week==0) {
+	console.log("Sunday");
 }
-else{
-	console.log("False");
+else if (week==1) {
+	console.log("Monday");
 }
-
+else if (week==2) {
+        console.log("Tuesday");
+}
+else if (week==3) {
+        console.log("Wednesday");
+}
+else if (week==4) {
+        console.log("Thursday");
+}
+else if (week==5) {
+        console.log("Friday");
+}
+else if (week==6) {
+        console.log("Saterday");
+}
+else {
+	console.log("Invalid.");
+}
---
+++ JavaScript/for_loop.js
@@ -0,0 +1,5 @@
+n=10
+
+for(i=0;i<=n;i++){
+	console.log(i);
+}
